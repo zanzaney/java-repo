7f7047597f6a792993b5834d17f8e6950632e3a1
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-30 00:59:11 +0530

    Create print-n-fibonacci.java

---
+++ print-n-fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int a = 0;//first number in fibonacci is always 0
+    int b = 1;//second number in fibonacci is always 1
+    for (int i=0;i<n;i++){
+      System.out.println(a);
+      int c = a+b;
+      a = b;
+      b=c;
+    }
+    }
+    
+  
+}
