a798b92a5d7408520e4c8a72a8dcaa657d9622d2
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-29 16:46:01 +0530

    Create postfix.java

---
+++ postfix.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+      int x = 10;
+      int y=x++;
+      System.out.println("The value of x is: "+x);
+      System.out.println("The value of y is: "+y);
+     }
+}
