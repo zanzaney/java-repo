47f90b174d670489628b7a7047dd5a84ddd2ce4b
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-29 14:21:01 +0530

    Create if-else.java

---
+++ if-else.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Main{
+  public static void main(String[] args){
+    int i = 1;
+    while(i<=9){
+      
+      System.out.println(i);
+      i++;
+    }
+    System.out.println("Done!");
+  }
+}
