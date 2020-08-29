9d80c70a57c68d5f83958cb7a191fd867cde1af8
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-29 11:03:20 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    int x = 11;
+    if (x%2 == 0){
+      System.out.println("It is an even number");
+    }
+    else{
+      System.out.println("It is an odd number");
+    }
+  }
+}
