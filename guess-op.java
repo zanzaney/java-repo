8e2e72e84737778e5904e3804c2058ec3bb5dc5d
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-29 16:52:31 +0530

    Create guess-op.java

---
+++ guess-op.java
@@ -0,0 +1,13 @@
+// Guess the o/p for the following code
+
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+    int i = 10;
+    for(;i<=10;){
+        System.out.println(i);
+        i++;
+    }
+    }
+}
