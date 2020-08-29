5072dea7944e67008c6548b77151f927b17fe410
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-08-29 17:25:05 +0530

    Create inputs.java

---
+++ inputs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+        System.out.println("Hi "+name+" !");
+    }
+        
+    
+}
