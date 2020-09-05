697f9bc13fa749eac25c0ca049e3279d7e050fa4
Archishman Zanzaney <43935752+zanzaney@users.noreply.github.com>
2020-09-05 13:14:04 +0530

    Create pattern-7.java

---
+++ pattern-7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        
+	    
+	    int n = scn.nextInt();
+	    int sp = 0;
+	    int st = 1;
+	    for(int i =1;i<=n;i++){
+	        for(int j=1;j<=sp;j++){
+	            System.out.print("\t");
+	        }
+	        System.out.print("*\t");
+	        sp++;
+	        System.out.println();
+	    }
+
+    }
+}
